94b35b9b9428ab76836fcf71c00f0e123e291260
Dmitry Afanassyev <attflederx@gmail.com>
2017-11-27 23:17:11 +0200

    Added data structures & log handler classes & modified utils class

---
+++ MatrixCalculator/MatrixCalculator/LogHandler.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MatrixCalculator.DataStructures;
+
+namespace MatrixCalculator
+{
+    /// <summary>
+    /// Class for handling the log operations
+    /// </summary>
+    class LogHandler
+    {
+        public TextBox LogTBox { get; }
+
+        public LogHandler(TextBox logTBox)
+        {
+            LogTBox = logTBox;
+        }
+
+        /// <summary>
+        /// Prints the message into the log textbox
+        /// </summary>
+        /// <param name="msg"></param>
+        public void LogMessage(string msg)
+        {
+            LogTBox.AppendText(string.Format(" >>> {0}\r\n", msg));
+        }
+
+        /// <summary>
+        /// Prints the matrix into the log textbox
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void LogMatrix(Matrix matrix)
+        {
+            string[] matrixRows = matrix.ToString().Split('\n'); // split by whitespaces
+            int[] maxLengths = MaxElemLengthByCol(matrixRows);
+
+            LogTBox.AppendText("\r\n");
+            foreach (string row in matrixRows)
+            {
+                string[] elems = row.Split(' '); // split by spaces
+                for (int i = 0; i < elems.Length; i++)
+                {
+                    LogTBox.AppendText(elems[i].PadRight(maxLengths[i] + 1)); // added padding
+                }
+                LogTBox.AppendText("\r\n");
+            }
+            LogTBox.AppendText("\r\n");
+        }
+
+        /// <summary>
+        /// Prints the system into the log textbox
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void LogSystem(LinearSystem system)
+        {
+            string[] systemRows = system.ToString().Split('\n'); // split by whitespaces
+            int[] maxLengths = MaxElemLengthByCol(systemRows);
+
+            LogTBox.AppendText("\r\n");
+            foreach (string row in systemRows)
+            {
+                if (row.Length == 0) { continue; } // string.Split() may return some empty substrings
+
+                string[] elems = row.Split(' '); // split by spaces
+                for (int i = 0; i < elems.Length - 1; i++) // exclude the constant term
+                {
+                    LogTBox.AppendText(elems[i].PadRight(maxLengths[i] + 1)); // added padding
+                }
+                LogTBox.AppendText(string.Format("| {0}\r\n", elems[elems.Length - 1].PadRight(maxLengths[maxLengths.Length - 1] + 1)));
+            }
+            LogTBox.AppendText("\r\n");
+        }
+
+        /// <summary>
+        /// Prints the vector into the log textbox
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void LogVector(double[] vector)
+        {
+            LogTBox.AppendText("\r\n[ ");
+            foreach (double val in vector)
+            {
+                LogTBox.AppendText(string.Format("{0} ", val.ToString()));
+            }
+            LogTBox.AppendText("]\r\n");
+        }
+
+        /// <summary>
+        /// Returns the maximum element length in the matrix(or system) for padding
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <returns></returns>
+        private int[] MaxElemLengthByCol(string[] rows)
+        {
+            int[] maxLengths;
+            string[][] elems = new string[rows.Length][]; // split the rows into elements
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                elems[i] = rows[i].Split(' ');
+            }
+
+            maxLengths = new int[elems[0].Length];
+            // this will also include the constant terms if a system is passed
+            for (int i = 0; i < elems[0].Length; i++)
+            {
+                maxLengths[i] = 0;
+                for (int j = 0; j < elems.Length; j++)
+                {
+                    if (elems[j][i].Length > maxLengths[i]) // string.Split() may return some empty substrings
+                    {
+                        maxLengths[i] = elems[j][i].Length;
+                    }
+                }
+            }
+
+            return maxLengths;
+        }
+    }
+}
--- MatrixCalculator/MatrixCalculator/Util.cs
+++ MatrixCalculator/MatrixCalculator/Util.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MatrixCalculator
 {
@@ -15,6 +16,26 @@ namespace MatrixCalculator
         public static string GetVersionString()
         {
             return "v" + typeof(Util).Assembly.GetName().Version.ToString();
+        }
+
+        /// <summary>
+        /// Displays an error box with a specified message
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult ErrorBox(string msg)
+        {
+            return MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Displays a confirmation box with a specified message
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static DialogResult ConfirmationBox(string msg)
+        {
+            return MessageBox.Show(msg, "Exit", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
         }
     }
 }
